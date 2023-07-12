e4ada514431f2191bbc1d0ed17bbfceb87134dce
Matthew Shaw <mshaw.cx@gmail.com>
2023-07-12 14:46:12 -0400

    fix manifest and setup.py for markdown

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ import setuptools
 exec(compile(open('updatedns/version.py').read(),'version.py','exec'))
 
 setuptools.setup(
-    name                 = 'updatedns',
-    author               = __author__,
-    author_email         = __email__,
-    version              = __version__,
-    license              = __license__,
-    url                  = 'https://shaw.cx/updatedns',
-    description          = 'Command-Line Cloud DNS utility',
-    long_description     = open('README.md').read(),
-    packages             = setuptools.find_packages(),
+    name = 'updatedns',
+    author       = __author__,
+    author_email = __email__,
+    version      = __version__,
+    license      = __license__,
+    url = 'https://shaw.cx/updatedns',
+    description = 'Command-Line Cloud DNS utility',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    packages = setuptools.find_packages(),
     include_package_data = True,
-    zip_safe             = False,
+    zip_safe = False,
     install_requires = [
         'apache-libcloud',
         ],
